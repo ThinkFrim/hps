f43df36c2b5f70047ef536f8351382eeddc585c3
firas halabi <81621892+firashalabi@users.noreply.github.com>
2023-09-21 09:39:49 +0300

    Comitting my changes in src/App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Nav from "./components/nav/Nav";
-
+import Cards from "./components/cards/Cards";
 const App = () => {
   return (
     <div>
       <Nav />
+      <Cards />
     </div>
   );
 };
